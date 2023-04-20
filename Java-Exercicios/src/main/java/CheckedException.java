060085e8728791e7324e0dbabfd36e6fef13e7aa
Humberto Freitas <95453824+HumbertoFF@users.noreply.github.com>
2023-04-19 21:21:15 -0300

    tratando erros esperados, usando classe para leitura de arquivos de texto.

---
+++ Java-Exercicios/src/main/java/CheckedException.java
@@ -0,0 +1,41 @@
+package main.java;
+
+import javax.swing.*;
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args){
+        //variável com o nome do arquivo para ser passado pelo método 'nomeDoArquivo'.
+        String nomeDoArquivo = "arquivoDeTexto.txt";
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        }catch ( FileNotFoundException e){
+            //tratando erro de arquivo não encontrado.
+            JOptionPane.showMessageDialog(null,"Verifique o nome do arquivo");
+        }
+        catch (IOException e) {
+            //Erro genérico que não dá pra saber o que vai acontecer.
+            JOptionPane.showMessageDialog(null,"O correu um erro inesperado");
+        }
+
+        System.out.println("continua o programa");
+    }
+
+    private static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+        //cria um arquivo qualquer e o nomeia
+        File file = new File(nomeDoArquivo);
+        //Realiza um Buffered de leitura para o arquivo informado
+        BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+        String line = br.readLine();
+        //Realiza um Buffered de escrita para o arquivo informado
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line=br.readLine();
+        } while(line != null);
+        bw.flush();
+        br.close();
+    }
+}
